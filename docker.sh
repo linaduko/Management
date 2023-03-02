936ffc4e41028cd34a7db9923b762abc9ce27f05
Lina <Lina>
2023-03-02 17:31:03 +0300

    #!/bin/bash
    apt-get remove docker docker-engine docker.io containerd runc
    
    wget -O containerd.io.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.6.9-1_amd64.deb
    wget -O docker-ce.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_23.0.1-1~ubuntu.20.04~focal_amd64.deb
    wget -O docker-ce-cli.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_23.0.1-1~ubuntu.20.04~focal_amd64.deb
    wget -O docker-buildx-plugin.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-buildx-plugin_0.10.2-1~ubuntu.20.04~focal_amd64.deb
    wget -O docker-compose-plugin.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-compose-plugin_2.6.0~ubuntu-focal_amd64.deb
    
    dpkg -i ./containerd.io.deb \
    ./docker-ce.deb \
    ./docker-ce-cli.deb \
    ./docker-buildx-plugin.deb \
    ./docker-compose-plugin.deb
    
    service docker start
    systemctl enable docker.service
    git systemctl enable containerd.service
    
    groupadd docker
    usermod -aG docker

---
+++ docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt-get remove docker docker-engine docker.io containerd runc
+
+wget -O containerd.io.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.6.9-1_amd64.deb
+wget -O docker-ce.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_23.0.1-1~ubuntu.20.04~focal_amd64.deb
+wget -O docker-ce-cli.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_23.0.1-1~ubuntu.20.04~focal_amd64.deb
+wget -O docker-buildx-plugin.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-buildx-plugin_0.10.2-1~ubuntu.20.04~focal_amd64.deb
+wget -O docker-compose-plugin.deb https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-compose-plugin_2.6.0~ubuntu-focal_amd64.deb
+
+dpkg -i ./containerd.io.deb \
+./docker-ce.deb \
+./docker-ce-cli.deb \
+./docker-buildx-plugin.deb \
+./docker-compose-plugin.deb
+
+service docker start
+systemctl enable docker.service
+systemctl enable containerd.service
+
+groupadd docker
+usermod -aG docker $USER
